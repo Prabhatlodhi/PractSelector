a7b0b616243c60fafb2e0e859874aea7bc1ea620
Prabhat Kumar Lodhi <114669921+Prabhatlodhi@users.noreply.github.com>
2022-11-15 01:46:46 -1200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+
+
+
+
+
+
+// // let a = alert ('hello');
+// // console.log(a)
+// // confirm
+
+// // let name = prompt('What is your name?', "Guest")
+// // console.log(name);
+
+// // let deletePost = confirm('Do you really want to delete this poast?');
+// // console.log(deletePost);
+
+// // if(deletePost){
+// //     console.log('uuuuuuuuuuuuuuuu')
+// // }else{
+// //     console.log('kkkkkkkkkkkkkkkk')
+// // }
+// // console.log(deletePost);
+
+// function myfunction(){
+//     alert('Hello I am a loda')
+// }
+
+// document.getElementById("myBtn").addEventListener("click", function() {
+//     alert("Hello World!");
+//   });
+
+
+//   function hide(){
+//    const btn = document.getElementById('kibtn');
+//    const para = document.getElementById('para');
+//    if(para.style.visibility != 'hidden'){
+//     para.style.visibility = "hidden";
+//    }
+//    else{
+//     para.style.visibility = "visible";
+//    }
+//   } 
+
+
+//   const para = document.getElementById('para');
+//   para.addEventListener('mouseover', function run(){
+//     console.log('MouseInside')
+
+//   });
+
+//   para.addEventListener('mouseout', function run(){
+//     console.log('MouseOut')
+
+//   });
+
+
+
+//   function greet (name){
+//     console.log('Hello Good Morning ' + name);
+//   }
+   
+
+// setTimeout(greet(' kiki'), 5000);
+
+
+
+// // let aa = document.getElementsByClassName('container')[0];
+// // console.log(aa)
